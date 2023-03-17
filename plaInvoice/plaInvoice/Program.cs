75b0d53f997fc02736917a398eb50816418ebde6
Gabriel Faria <gabriel@goncalvesfaria.com>
2023-03-17 22:19:32 +0100

    Initial version - just learning the library (System.CommandLine)

--- plaInvoice/plaInvoice/Program.cs
+++ plaInvoice/plaInvoice/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace plaInvoice
+﻿using System.IO;
+using System.CommandLine;
+
+namespace plaInvoice
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var invoiceValue = new Option<decimal>(new string[] { "-a", "--amount" }, "Gross amount to be charged in this invoice");
+            var companyToCharge = new Option<string>(new string[] { "-c", "--company" }, "Company that's going to be charged");
+
+            var rootCommand = new RootCommand("Simple invoice generator");
+
+            var generateInvoiceCommand = new Command("generate", "Generates an invoice with the provided parameters")
+            {
+                invoiceValue,
+                companyToCharge
+            };
+            rootCommand.AddCommand(generateInvoiceCommand);
+
+            generateInvoiceCommand.SetHandler((amount, company) =>
+            {
+                PrintValue(amount, company);
+            }, invoiceValue, companyToCharge);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        static void PrintValue(decimal value, string company)
+        {
+            Console.WriteLine($"Congrats, {company} will pay you {value} dollars, you rich motherfucker!!!");
         }
     }
 }
