c84f08fb15c2bf83911ebb6d6fcee20531a7c27c
Gabriel Faria <gabriel@goncalvesfaria.com>
2023-03-19 14:26:20 +0100

    Add PDF capabilities with SyncFusion

--- plaInvoice/plaInvoice/Program.cs
+++ plaInvoice/plaInvoice/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using Microsoft.Extensions.Configuration;
+using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
 using System.CommandLine;
 
 namespace plaInvoice
@@ -7,6 +9,8 @@ namespace plaInvoice
     {
         static async Task<int> Main(string[] args)
         {
+            ConfigureSyncFusion(args);
+            
             var invoiceValue = new Option<decimal>(new string[] { "-a", "--amount" }, "Gross amount to be charged in this invoice");
             var companyToCharge = new Option<string>(new string[] { "-c", "--company" }, "Company that's going to be charged");
 
@@ -27,9 +31,31 @@ namespace plaInvoice
             return await rootCommand.InvokeAsync(args);
         }
 
+        private static void ConfigureSyncFusion(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json")
+              .Build();
+
+            var syncFusionKey = configuration.GetValue<string>("syncfusionApplicationKey");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncFusionKey);
+        }
+
         static void PrintValue(decimal value, string company)
         {
-            Console.WriteLine($"Congrats, {company} will pay you {value} dollars, you rich motherfucker!!!");
+            //Initialize HTML to PDF converter
+            HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+
+            //HTML string and Base URL 
+            string htmlText = $"<html><body><h1>Congrats, {company} will pay you {value} dollars, you rich motherfucker!!!</h1></body></html>";
+            string baseUrl = @"C:/Temp/HTMLFiles/";
+
+            //Convert URL to PDF
+            PdfDocument document = htmlConverter.Convert(htmlText, baseUrl);
+            FileStream fileStream = new FileStream("HTML-to-PDF.pdf", FileMode.CreateNew, FileAccess.ReadWrite);
+            //Save and close the PDF document.
+            document.Save(fileStream);
+            document.Close(true);
         }
     }
 }
