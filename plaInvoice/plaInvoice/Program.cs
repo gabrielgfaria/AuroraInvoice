44b6b8bf3b8e7c84825c2771b59adc55a010711b
Gabriel Faria <gabriel@goncalvesfaria.com>
2023-03-19 22:14:46 +0100

    First working version

--- plaInvoice/plaInvoice/Program.cs
+++ plaInvoice/plaInvoice/Program.cs
@@ -7,55 +7,95 @@ namespace plaInvoice
 {
     internal class Program
     {
+        static IConfiguration _configuration;
+        
         static async Task<int> Main(string[] args)
         {
+            SetConfiguration();
             ConfigureSyncFusion(args);
             
             var invoiceValue = new Option<decimal>(new string[] { "-a", "--amount" }, "Gross amount to be charged in this invoice");
             var companyToCharge = new Option<string>(new string[] { "-c", "--company" }, "Company that's going to be charged");
+            var outputPath = new Option<string>(new string[] { "-o", "--output" }, "[optional] File output path");
 
             var rootCommand = new RootCommand("Simple invoice generator");
 
             var generateInvoiceCommand = new Command("generate", "Generates an invoice with the provided parameters")
             {
                 invoiceValue,
-                companyToCharge
+                companyToCharge,
+                outputPath
             };
             rootCommand.AddCommand(generateInvoiceCommand);
 
-            generateInvoiceCommand.SetHandler((amount, company) =>
+            generateInvoiceCommand.SetHandler((amount, company, outputPath) =>
             {
-                PrintValue(amount, company);
-            }, invoiceValue, companyToCharge);
+                PrintValue(amount, company, outputPath);
+            }, invoiceValue, companyToCharge, outputPath);
 
             return await rootCommand.InvokeAsync(args);
         }
 
-        private static void ConfigureSyncFusion(string[] args)
+        private static void SetConfiguration()
         {
-            IConfiguration configuration = new ConfigurationBuilder()
+            _configuration = new ConfigurationBuilder()
               .AddJsonFile("appsettings.json")
               .Build();
+        }
 
-            var syncFusionKey = configuration.GetValue<string>("syncfusionApplicationKey");
+        private static void ConfigureSyncFusion(string[] args)
+        {
+            var syncFusionKey = _configuration.GetValue<string>("syncfusionApplicationKey");
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncFusionKey);
         }
 
-        static void PrintValue(decimal value, string company)
+        static void PrintValue(decimal value, string company, string outputPath)
         {
-            //Initialize HTML to PDF converter
+            ////Initialize HTML to PDF converter
             HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+            BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+            blinkConverterSettings.ViewPortSize = new Syncfusion.Drawing.Size(1024, 0);
+            htmlConverter.ConverterSettings = blinkConverterSettings;
 
-            //HTML string and Base URL 
-            string htmlText = $"<html><body><h1>Congrats, {company} will pay you {value} dollars, you rich motherfucker!!!</h1></body></html>";
-            string baseUrl = @"C:/Temp/HTMLFiles/";
+            ////HTML string and Base URL 
+            string htmlText = GetHtml(value, company);
+            string baseUrl = Path.GetFullPath("../../../Resources/");
 
-            //Convert URL to PDF
+            ////Convert URL to PDF
             PdfDocument document = htmlConverter.Convert(htmlText, baseUrl);
-            FileStream fileStream = new FileStream("HTML-to-PDF.pdf", FileMode.CreateNew, FileAccess.ReadWrite);
-            //Save and close the PDF document.
+            FileStream fileStream = new FileStream(GetFileOutputPath(outputPath, company), FileMode.CreateNew, FileAccess.ReadWrite);
+            
+            ////Save and close the PDF document.
             document.Save(fileStream);
             document.Close(true);
+        }
+
+        private static string GetFileOutputPath(string outputPath, string company)
+        {
+            if (!string.IsNullOrWhiteSpace(outputPath))
+                return outputPath;
+
+            var rootPath = _configuration.GetValue<string>("rootPathForFileOutput");
+            var date = DateTime.Now;
+
+            if (company.ToLower() == "number8" || company.ToLower() == "number 8")
+                return rootPath + $"Number8/Invoices/AURORA TI LTDA_invoice_{date:yyyy_MM_dd}.pdf";
+            else if (company.ToLower() == "toptal")
+                return rootPath + $"Toptal/Invoices/AURORA TI LTDA_invoice_{date:yyyy_MM_dd}.pdf";
+            else
+                return "default.pdf";
+        }
+
+        private static string GetHtml(decimal value, string company)
+        {
+            var path = Path.GetFullPath("../../../Resources/invoiceTemplate.html");
+            var html = File.ReadAllText(path);
+
+            html = html.Replace("KEY_COMPANY", company.ToUpper());
+            html = html.Replace("KEY_AMOUNT", value.ToString("#,##0.00"));
+            html = html.Replace("KEY-DATE", DateTime.Now.ToString("MMM dd, yyyy"));
+
+            return html;
         }
     }
 }
